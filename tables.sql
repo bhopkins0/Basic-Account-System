a75900ebb8558143413cda3c36aa6da1528d2abb
Brent Hopkins <contact@brenthopkins.me>
2022-11-04 02:05:18 -0500

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -3,12 +3,12 @@ CREATE TABLE `users` (
 	`email` VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
 	`password` VARCHAR(75) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
 	`creation_time` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-	`creation_ip` INT(28),
+	`creation_ip` BIGINT,
 	PRIMARY KEY (acc_id)
 );
 CREATE TABLE `login_attempts` (
 	`acc_id` VARCHAR(32) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
 	`is_successful` VARCHAR(16) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
 	`login_time` VARCHAR(64) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL,
-	`login_ip` INT(28)
+	`login_ip` BIGINT
 );
